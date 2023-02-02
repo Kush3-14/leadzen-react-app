9f26233b8b1bad2b5da82e6391be29daaaaf112a
Kush <kush3_14@outlook.com>
2023-02-02 17:52:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUsers(result.data);
+    };
+    fetchData();
+  }, []);
+
+  const handleViewDetails = (user) => {
+    setSelectedUser(user);
+  };
+
+  const handleHideDetails = () => {
+    setSelectedUser(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table class="table table-borderless">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>
+                {selectedUser && selectedUser.id === user.id ? (
+                  <button type="button" class="btn btn-danger" onClick={handleHideDetails}>Hide Details</button>
+                ) : (
+                  <button type="button" class="btn btn-danger" onClick={() => handleViewDetails(user)}>View Details</button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {selectedUser && (
+        <div>
+          <h2>User Details</h2>
+          <p>ID: {selectedUser.id}</p>
+          <p>Name: {selectedUser.name}</p>
+          <p>Username: {selectedUser.username}</p>
+          <p>Email: {selectedUser.email}</p>
+          <p>Phone: {selectedUser.phone}</p>
+          <p>Website: {selectedUser.website}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
